886a3523479d159207ea67d6d23d59ef9cbf84ac
Ebenezer Don <ebenezerdonu@gmail.com>
2021-04-29 17:55:35 +0100

    add working code to example file

--- example/src/app.js
+++ example/src/app.js
@@ -2,4 +2,29 @@ import React from 'react'
 import { render } from 'react-dom'
 import ChatBox from '../../src'
 
-render(<ChatBox />, document.getElementById('root'))
+const App = () => {
+  const [messageList, setMessageList] = React.useState([])
+
+  const handleSendMessage = newMessage => {
+    setMessageList([
+      ...messageList,
+      {text: newMessage, person: 'primary'}
+    ])
+  }
+
+  return (
+    <div>
+      <ChatBox
+        settings={{
+          position: 'right',
+          navColor: 'green',
+          navText: 'Jane Doe',
+        }}
+        messageList={messageList}
+        onSendMessage={handleSendMessage}
+      />
+    </div>
+  )
+}
+
+render(<App />, document.getElementById('root'))
